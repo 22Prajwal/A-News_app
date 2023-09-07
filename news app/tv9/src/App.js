a85aae3c33ce7050a239f8c56d2c8eec5ff016a0
PRAJWAL P KURDEKAR <prajwalpkurdekar522@gmail.com>
2023-09-07 15:57:55 +0530

    React news app

---
+++ news app/tv9/src/App.js
@@ -0,0 +1,14 @@
+import News from './News';
+import './App.css';
+import React from 'react';
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       <News/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
